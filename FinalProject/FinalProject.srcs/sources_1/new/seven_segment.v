fb60f57d0150f8f75a295074769e9e10192d77e8
ayak2002 <ayak@bu.edu>
2021-11-30 15:26:30 -0500

    Update seven_segment.v

--- FinalProject/FinalProject.srcs/sources_1/new/seven_segment.v
+++ FinalProject/FinalProject.srcs/sources_1/new/seven_segment.v
@@ -1,35 +1,38 @@
-// Takes in encoded 7-segment value
-module seven_segment_decoder(num, display);
-    input [3:0] num;
-    output [6:0] display;
-    reg [6:0] z,display; 
-    // Seven seg display combinations for each 4 bit number
-    //0 thru f: 0 to 9 & a to f
-    always @ (*) begin
-        case (num)
-            4'b0000 : z = 7'b1111110;
-            4'b0001 : z = 7'b0110000;
-            4'b0010 : z = 7'b1101101; 
-            4'b0011 : z = 7'b1111001;
-            4'b0100 : z = 7'b0110011;
-            4'b0101 : z = 7'b1011011;  
-            4'b0110 : z = 7'b1011111;
-            4'b0111 : z = 7'b1110000;
-            4'b1000 : z = 7'b1111111;
-            4'b1001 : z = 7'b1111011;
-            4'b1010 : z = 7'b1110111; 
-            4'b1011 : z = 7'b0011111;
-            4'b1100 : z = 7'b1001110;
-            4'b1101 : z = 7'b0111101;
-            4'b1110 : z = 7'b1001111;
-            4'b1111 : z = 7'b1000111;
+//Code for seven segment decoder
+module seven_segment_decoder(
+    select_i,
+    count_i,
+    seg_o);
+    
+    input [1:0] select_i;
+    input [11:0] num_i;
+    output reg [6:0] seg_o;
+    reg [3:0] d_i;
+    
+    always @(*) begin
+        case (select_i)
+            2'b00: d_i = num_i[3:0];
+            2'b01: d_i = num_i[7:4];
+            2'b10: d_i = num_i[11:8];
         endcase
-        display[0] = ~z[0];
-        display[1] = ~z[1];
-        display[2] = ~z[2];
-        display[3] = ~z[3];
-        display[4] = ~z[4];
-        display[5] = ~z[5];
-        display[6] = ~z[6];  
-    end    
+        case (d_i)
+            4'b0000: seg_o = 7'b0000001;
+            4'b0001: seg_o = 7'b1001111;
+            4'b0010: seg_o = 7'b0010010;
+            4'b0011: seg_o = 7'b0000110;
+            4'b0100: seg_o = 7'b1001100;
+            4'b0101: seg_o = 7'b0100100;
+            4'b0110: seg_o = 7'b0100000;
+            4'b0111: seg_o = 7'b0001111;
+            4'b1000: seg_o = 7'b0000000;
+            4'b1001: seg_o = 7'b0000100;
+            4'b1010: seg_o = 7'b0001000;
+            4'b1011: seg_o = 7'b1100000;
+            4'b1100: seg_o = 7'b0110001;
+            4'b1101: seg_o = 7'b1000010;
+            4'b1110: seg_o = 7'b0110000;
+            4'b1111: seg_o = 7'b0111000;
+         endcase
+    end
+          
 endmodule
