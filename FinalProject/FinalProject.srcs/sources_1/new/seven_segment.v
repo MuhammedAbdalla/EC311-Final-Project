466db4502edc3c30d314f92e429f44c5b27788b6
ayak2002 <ayak@bu.edu>
2021-11-30 15:49:06 -0500

    Update seven_segment.v

--- FinalProject/FinalProject.srcs/sources_1/new/seven_segment.v
+++ FinalProject/FinalProject.srcs/sources_1/new/seven_segment.v
@@ -1,11 +1,13 @@
-//Code for seven segment decoder
+//Code for 7 segment decoder
 module seven_segment_decoder(
     select_i,
     num_i,
+    count5_i,
     seg_o);
     
     input [1:0] select_i;
-    input [11:0] num_i;
+    input [7:0] num_i;
+    input [3:0] count5_i;
     output reg [6:0] seg_o;
     reg [3:0] d_i;
     
@@ -13,7 +15,7 @@ module seven_segment_decoder(
         case (select_i)
             2'b00: d_i = num_i[3:0];
             2'b01: d_i = num_i[7:4];
-            2'b10: d_i = num_i[11:8];
+            2'b10: d_i = count5_i;
         endcase
         case (d_i)
             4'b0000: seg_o = 7'b0000001;
