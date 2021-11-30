0815b01d49659936b8fe0e9a81d37183ebb050be
ayak2002 <ayak@bu.edu>
2021-11-30 15:17:18 -0500

    binary_to_bcd.v completed

--- FinalProject/FinalProject.srcs/sources_1/new/binary_to_bcd.v
+++ FinalProject/FinalProject.srcs/sources_1/new/binary_to_bcd.v
@@ -20,7 +20,31 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module binary_to_bcd(
-
-    );
+module binary_to_bcd(binary_in, bcd_out);
+    input [5:0] binary_in;
+    output reg [7:0] bcd_out = 8'b0;
+    reg [3:0] ones = 4'b0000;
+    reg [3:0] tens = 4'b0000;
+    reg [5:0] binary_shift;
+    reg [2:0] i;
+    parameter MAX = 3'b101;
+    
+    always @(binary_in) begin
+        ones = 4'b0000;
+        tens = 4'b0000;
+        binary_shift = binary_in;
+        for(i = 0; i < 3'b110; i = i+1'b1) begin
+            if(ones >= MAX) begin
+                ones = ones + 2'b11;
+            end
+            if(tens >= MAX) begin
+                tens = tens + 2'b11;
+            end
+            tens = {tens[2:0], ones[3]};
+            ones = {ones[2:0], binary_shift[5]};
+            binary_shift = {binary_shift[4:0], 1'b0};
+            bcd_out = {tens, ones};
+        end
+    end //always
+    
 endmodule
