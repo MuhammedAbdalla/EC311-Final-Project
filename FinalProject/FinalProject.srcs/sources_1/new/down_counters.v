4240f9f9709166b7d76633fa9b755b79d2697066
ayak2002 <ayak@bu.edu>
2021-11-30 18:50:04 -0500

    down counters are now two modules

--- FinalProject/FinalProject.srcs/sources_1/new/down_counters.v
+++ FinalProject/FinalProject.srcs/sources_1/new/down_counters.v
@@ -1,39 +1,42 @@
 `timescale 1ns / 1ps
 
-module down_counters(clk_hz, reset, count5_out, count30_out);
+module down_counter5(clk_hz, reset, count5_out, count5_enable);
     input clk_hz, reset;
-    output count5_out, count30_out;
+    output reg [2:0] count5_out = 3'b101;
+    output reg count5_enable = 1'b0;
     
-    parameter [2:0] five = 3'b101;
-    parameter [4:0] thirty = 5'b11110;
-    
-    reg count5_out, count30_out;
-    reg [2:0] downcount5 = five;
-    reg [4:0] downcount30 = thirty;
     
     // count down from 5
     always @ (posedge clk_hz or negedge reset) begin
-        if (reset == 0 | count5_out == 1'b1) begin
-            downcount5 = five;
-            count5_out = 1'b0;
+        if (reset == 1'b0) begin
+            count5_out = 3'b101;
+            count5_enable = 1'b0;
         end else begin
-            downcount5 = downcount5 - 1'b1;
-            if (downcount5 == 0) begin
-                count5_out = 1'b1;
+            count5_out = count5_out - 1'b1;
+            if (count5_out == 0) begin
+                count5_enable = 1'b1;
             end
         end
     end
     
+ 
+ module down_counter30(clk_hz, reset, count30_enable);
+    input clk_hz, reset;
+    output reg count30_enable = 1'b0;
+    reg [4:0] count30 = 5'b11110;
+    
+    
     // count down from 30
     always @ (posedge clk_hz or negedge reset) begin
-        if (reset == 0 | count30_out == 1'b1) begin
-            downcount30 = thirty;
-            count30_out = 1'b0;
+        if (reset == 1'b0) begin
+            count30 = 5'b11110;
+            count30_enable = 1'b0;
         end else begin
-            downcount30 = downcount30 - 1'b1;
-            if (downcount30 == 0) begin
-                count30_out = 1'b1;
+            count30 = count30 - 1'b1;
+            if (count30 == 0) begin
+                count30_enable = 1'b1;
             end
         end
     end
-endmodule
+    
+ 
