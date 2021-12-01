236054de5f4de93e1ffc94358ff7b534d4790ed2
ayak2002 <ayak@bu.edu>
2021-11-30 20:24:58 -0500

    Update display_control.v

--- FinalProject/FinalProject.srcs/sources_1/new/display_control.v
+++ FinalProject/FinalProject.srcs/sources_1/new/display_control.v
@@ -2,26 +2,22 @@
 //EDIT DISPLAY CONTROL TO SUPPORT 1KHZ REFRESH FOR ALL 8 7-SEGMENTS
 // Display Control module controls the seven segement display with
 // a refresh rate of 1kHz for all 4 of the seven seg displays
-module display_control(reset_i, count2, digit_select_o);
-    input reset_i;
-    input [1:0] count2;
-    output reg [2:0] digit_select_o;
-    
-    parameter [2:0] sevSeg1 = 3'b110;
-    parameter [2:0] sevSeg2 = 3'b101;
-    parameter [2:0] sevSeg3 = 3'b011;
 
-    always @ (*) begin
-        if (reset_i == 0) begin
-            digit_select_o = 3'b111;
-        end else begin
-            if (count2 == 2'b00) begin
-                digit_select_o = sevSeg1;
-            end else if (count2 == 2'b01) begin
-                digit_select_o = sevSeg2;
-            end else if (count2 == 2'b10) begin
-                digit_select_o = sevSeg3;
-            end
-        end
-    end  
+//Code for display control module
+module display_control(
+    select_i,
+    digit_select_o);
+    
+    input [1:0] select_i;
+    output [3:0] digit_select_o;
+    reg [3:0] digit_select_o;
+    
+    always @(*) begin
+        case (select_i)
+            2'b00: digit_select_o = 4'b1110;
+            2'b01: digit_select_o = 4'b1101;
+            2'b10: digit_select_o = 4'b1011;
+            2'b11: digit_select_o = 4'b0111;
+        endcase
+    end//always
 endmodule
