69aec5e438385770dd3a5519842c3b4f532803fd
ayak2002 <ayak@bu.edu>
2021-11-30 19:25:32 -0500

    Update down_counters.v

--- FinalProject/FinalProject.srcs/sources_1/new/down_counters.v
+++ FinalProject/FinalProject.srcs/sources_1/new/down_counters.v
@@ -21,25 +21,27 @@ module down_counter5(clk_hz, reset, count5_out, count5_enable);
 endmodule
     
  
- module down_counter30(clk_hz, reset, count30_enable);
+
+ module down_counter5(clk_hz, reset, count30_out, count30_enable);
     input clk_hz, reset;
+    output reg [4:0] count30_out = 5'b11110;
     output reg count30_enable = 1'b0;
-    reg [4:0] count30 = 5'b11110;
     
     
     // count down from 30
     always @ (posedge clk_hz or negedge reset) begin
         if (reset == 1'b0) begin
-            count30 = 5'b11110;
+            count30_out = 5'b11110;
             count30_enable = 1'b0;
         end else begin
-            count30 = count30 - 1'b1;
-            if (count30 == 0) begin
+            count30_out = count30_out - 1'b1;
+            if (count30_out == 0) begin
                 count30_enable = 1'b1;
             end
         end
     end
- endmodule
+endmodule
+    
  
     
  
